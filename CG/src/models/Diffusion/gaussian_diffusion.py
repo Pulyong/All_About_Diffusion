854aa6987ce1e550c414f0028cca607c7f5afc20
Pulyong <rla020@naver.com>
2024-05-14 15:27:43 +0900

    re implementation

--- CG/src/models/Diffusion/gaussian_diffusion.py
+++ CG/src/models/Diffusion/gaussian_diffusion.py
@@ -137,11 +137,168 @@ class GaussianDiffusion():
                 max_log = _extract_into_tensor(
                     np.log(self.betas, t, x.shape)
                 )
+                # The model_var_values is [-1, 1] for [min_var, max_var].
+                frac = (model_var_values + 1) / 2
+                model_log_variance = frac * max_log + (1 - frac) * min_log
+                model_variance = torch.exp(model_log_variance)
+        else: # not learned Variance
+            model_variance, model_log_variance = {
+                ModelVarType.FIXED_LARGE:(
+                    np.append(self.posterior_variance[1], self.betas[1:]),
+                    np.log(np.append(self.posterior_variance[1], self.betas[1:]))
+                ),
+                ModelVarType.FIXED_SMALL:(
+                    self.posterioir_variance,
+                    self.posterior_log_variance_clipped
+                )
+            }[self.model_var_type]
+            model_variance = _extract_into_tensor(model_variance, t, x.shape)
+            model_variance = _extract_into_tensor(model_log_variance, t, x.shape)
             
+        def process_xstart(x):
+            if denoised_fn is not None:
+                x = denoised_fn(x)
+            if clip_denoised:
+                return x.clamp(-1,1)
+            return x
+        
+        if self.model_mean_type == ModelMeanType.PREVIOUS_X:
+            pred_xstart = process_xstart(
+                self._predict_xstart_from_xprev(x_t=x, t=t, xprev=model_output)
+            )
+            model_mean = model_output
+            
+        elif self.model_mean_type in [ModelMeanType.START_X, ModelMeanType.EPSILON]:
+            if self.model_mean_type == ModelMeanType.START_X:
+                pred_xstart = process_xstart(model_output)
+            else: # model predict epsilon
+                pred_xstart = process_xstart(
+                    self._predict_xstart_from_eps(x_t=x, t=t, eps=model_output)
+                )
+            model_mean, _, _ = self.q_posterior_mean_variance(
+                x_start=pred_xstart, x_t=x, t=t
+            )    
+        else:
+            raise NotImplementedError(self.model_mean_type)
+        
+        assert(
+            model_mean.shape == model_log_variance.shape == pred_xstart.shape == x.shape
+        )
+        return{
+            'mean': model_mean,
+            'variance': model_variance,
+            'log_variance': model_log_variance,
+            'pred_xstart': pred_xstart
+        }
+    
+    def _predict_xstart_from_eps(self, x_t, t, eps):
+        # from DDIM Eq 4
+        assert x_t.shape == eps.shape
+        return(
+            _extract_into_tensor(self.sqrt_recip_alphas_cumprod, t, x_t.shape) * x_t
+            - _extract_into_tensor(self.sqrt_recipm1_alphas_cumprod, t, x_t.shape) * eps
+        )
+    
+    def _predict_xstart_from_xprev(self, x_t, t, xprev):
+        assert x_t.shape == xprev.shape
+        # (xprev - coef2*x_t) / coef1
+        return (
+            _extract_into_tensor(1.0 / self.posterior_mean_coef1, t, x_t.shape) * xprev
+            - _extract_into_tensor(
+                self.posterior_mean_coef2 / self.posterior_mean_coef1, t, x_t.shape
+            ) * x_t
+        )
+        
+    def _predict_eps_from_xstart(self, x_t, t, pred_xstart):
+        return(
+            _extract_into_tensor(self.sqrt_recip_alphas_cumprod, t, x_t.shape) * x_t
+            - pred_xstart
+        ) / _extract_into_tensor(self.sqrt_recipm1_alphas_cumprod, t, x_t.shape)
+        
     def _scale_timesteps(self, t):
         if self.rescale_timesteps:
             return t.float() * (1000.0 / self.num_timesteps)
         return t
+    
+    # DDPM Eq 7
+    def p_sample(
+        self, model, x, t, clip_denoised=True, denoised_fn=None, model_kwargs=None
+    ):
+        out = self.p_mean_variance(
+            model,
+            x,
+            t,
+            clip_denoised=clip_denoised,
+            denoised_fn=denoised_fn,
+            model_kwargs=model_kwargs,
+        )
+        noise = torch.randn_like(x)
+        nonzero_mask = (
+            (t != 0).float().view(-1, *([1] * (len(x.shape) - 1)))
+        )  # no noise when t == 0
+        sample = out['mean'] + nonzero_mask * torch.exp(0.5 * out['log_variance']) * noise
+        return {"sample": sample, "pred_xstart": out["pred_xstart"]}
+    
+    def p_sample_loop(
+        self,
+        model,
+        shape,
+        noise=None,
+        clip_denoised=True,
+        denoised_fn=None,
+        model_kwargs=None,
+        device=None,
+        progress=False,
+    ):
+        final = None
+        for sample in self.p_sample_loop_progressive(
+            model, shape, noise=noise, clip_denoised = clip_denoised,
+            denoised_fn = denoised_fn, model_kwargs = model_kwargs,
+            device = device, progress = progress
+        ):
+            final = sample
+        return final['sample']
+    
+    def p_sample_loop_progressive(
+        self,
+        model,
+        shape,
+        noise=None,
+        clip_denoised=True,
+        denoised_fn=None,
+        model_kwargs=None,
+        device=None,
+        progress=False,
+    ):
+        if device is None:
+            device = next(model.parameters()).device
+        assert isinstance(shape, (tuple, list))
+        
+        if noise is not None:
+            img = noise
+        else:
+            img = torch.randn(*shape, device=device)
+        indices = list(range(self.num_timesteps))[::-1]
+        
+        if progress:
+            from tqdm.auto import tqdm
+            
+            indices = tqdm(indices)
+            
+        for i in indices:
+            t = torch.tensor([i] * shape[0], device=device)
+            with torch.no_grad():
+                out = self.p_sample(
+                    model,
+                    img,
+                    t,
+                    clip_denoised=clip_denoised,
+                    denoised_fn=denoised_fn,
+                    model_kwargs=model_kwargs,
+                )
+                yield out
+                img = out["sample"]
+        
 
 def _extract_into_tensor(arr, timesteps, broadcast_shape):
     '''
